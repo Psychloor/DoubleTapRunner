5e360db4c30f0386753fe703138653380c969a48
Kevin Blomqvist <blomqvistkevin@hotmail.com>
2021-02-08 12:20:19 +0100

    Updated to 1048

--- DoubleTapSpeed/DoubleTapper.cs
+++ DoubleTapSpeed/DoubleTapper.cs
@@ -12,6 +12,7 @@ namespace DoubleTapRunner
 
     using System;
     using System.Collections;
+    using System.Linq;
     using System.Reflection;
 
     using Harmony;
@@ -51,8 +52,8 @@ namespace DoubleTapRunner
         {
             if (instance != null)
             {
-                MelonLogger.LogError("There's already an instance of Double-Tap Runner. Remove the duplicate dll files");
-                MelonLogger.LogError("Not Guaranteed to work with multiple instances at all");
+                MelonLogger.Error("There's already an instance of Double-Tap Runner. Remove the duplicate dll files");
+                MelonLogger.Error("Not Guaranteed to work with multiple instances at all");
                 return;
             }
 
@@ -72,52 +73,55 @@ namespace DoubleTapRunner
                                      AxisClickThreshold = .6f
                                  };
 
-            MelonPrefs.RegisterCategory(SettingsCategory, "Double-Tap Runner");
-            MelonPrefs.RegisterBool(SettingsCategory, nameof(Settings.Enabled), activeSettings.Enabled, "Enabled");
-            MelonPrefs.RegisterFloat(SettingsCategory, nameof(Settings.SpeedMultiplier), activeSettings.SpeedMultiplier, "Speed Multiplier");
-            MelonPrefs.RegisterFloat(SettingsCategory, nameof(Settings.DoubleClickTime), activeSettings.DoubleClickTime, "Double Click Time");
+            MelonPreferences.CreateCategory(SettingsCategory, "Double-Tap Runner");
+            MelonPreferences.CreateEntry(SettingsCategory, nameof(Settings.Enabled), activeSettings.Enabled, "Enabled");
+            MelonPreferences.CreateEntry(SettingsCategory, nameof(Settings.SpeedMultiplier), activeSettings.SpeedMultiplier, "Speed Multiplier");
+            MelonPreferences.CreateEntry(SettingsCategory, nameof(Settings.DoubleClickTime), activeSettings.DoubleClickTime, "Double Click Time");
 
-            MelonPrefs.RegisterString(SettingsCategory, nameof(Settings.Forward), Enum.GetName(typeof(KeyCode), activeSettings.Forward), "Desktop Forward");
-            MelonPrefs.RegisterString(SettingsCategory, nameof(Settings.Backward), Enum.GetName(typeof(KeyCode), activeSettings.Backward), "Desktop Backward");
-            MelonPrefs.RegisterString(SettingsCategory, nameof(Settings.Left), Enum.GetName(typeof(KeyCode), activeSettings.Left), "Desktop Left");
-            MelonPrefs.RegisterString(SettingsCategory, nameof(Settings.Right), Enum.GetName(typeof(KeyCode), activeSettings.Right), "Desktop Right");
+            MelonPreferences.CreateEntry(SettingsCategory, nameof(Settings.Forward), Enum.GetName(typeof(KeyCode), activeSettings.Forward), "Desktop Forward");
+            MelonPreferences.CreateEntry(SettingsCategory, nameof(Settings.Backward), Enum.GetName(typeof(KeyCode), activeSettings.Backward), "Desktop Backward");
+            MelonPreferences.CreateEntry(SettingsCategory, nameof(Settings.Left), Enum.GetName(typeof(KeyCode), activeSettings.Left), "Desktop Left");
+            MelonPreferences.CreateEntry(SettingsCategory, nameof(Settings.Right), Enum.GetName(typeof(KeyCode), activeSettings.Right), "Desktop Right");
 
-            MelonPrefs.RegisterFloat(SettingsCategory, nameof(Settings.AxisDeadZone), activeSettings.AxisDeadZone, "Axis Dead Zone");
-            MelonPrefs.RegisterFloat(SettingsCategory, nameof(Settings.AxisClickThreshold), activeSettings.AxisClickThreshold, "Axis Click Threshold");
+            MelonPreferences.CreateEntry(SettingsCategory, nameof(Settings.AxisDeadZone), activeSettings.AxisDeadZone, "Axis Dead Zone");
+            MelonPreferences.CreateEntry(SettingsCategory, nameof(Settings.AxisClickThreshold), activeSettings.AxisClickThreshold, "Axis Click Threshold");
             ApplySettings();
 
             try
             {
                 // way more mod friendly but might mean more updates... oh well, not really hard to find. "Fade to" and check params
-                MethodInfo fadeToMethod = typeof(VRCUiManager).GetMethod(
-                    nameof(VRCUiManager.Method_Public_Void_String_Single_Action_0),
-                    BindingFlags.Public | BindingFlags.Instance);
-                harmonyInstance.Patch(
-                    fadeToMethod,
-                    null,
-                    new HarmonyMethod(typeof(DoubleTapper).GetMethod(nameof(JoinedRoomPatch), BindingFlags.NonPublic | BindingFlags.Static)));
+                var fadeMethods = typeof(VRCUiManager).GetMethods()
+                                                      .Where(
+                                                          m => m.Name.StartsWith("Method_Public_Void_String_Single_Action_") && m.GetParameters().Length == 3);
+                foreach (MethodInfo fadeMethod in fadeMethods)
+                {
+                    Harmony.Patch(
+                        fadeMethod,
+                        null,
+                        new HarmonyMethod(typeof(DoubleTapper).GetMethod(nameof(JoinedRoomPatch), BindingFlags.NonPublic | BindingFlags.Static)));
+                }
             }
             catch (Exception e)
             {
                 modLoadedCorrectly = false;
-                MelonLogger.LogError("Failed to patch into FadeTo: " + e);
+                MelonLogger.Error("Failed to patch into FadeTo: " + e);
             }
 
             try
             {
                 MethodInfo leaveRoomMethod = typeof(NetworkManager).GetMethod(nameof(NetworkManager.OnLeftRoom), BindingFlags.Public | BindingFlags.Instance);
-                harmonyInstance.Patch(
+                Harmony.Patch(
                     leaveRoomMethod,
                     new HarmonyMethod(typeof(DoubleTapper).GetMethod(nameof(LeftRoomPrefix), BindingFlags.NonPublic | BindingFlags.Static)));
             }
             catch (Exception e)
             {
                 modLoadedCorrectly = false;
-                MelonLogger.LogError("Failed to patch into OnLeftRoom: " + e);
+                MelonLogger.Error("Failed to patch into OnLeftRoom: " + e);
             }
 
             if (!modLoadedCorrectly)
-                MelonLogger.LogError("Didn't load in correctly, not guaranteed to fully work so i'll shutdown this mod");
+                MelonLogger.Error("Didn't load in correctly, not guaranteed to fully work so i'll shutdown this mod");
         }
 
         private static bool LeftRoomPrefix()
@@ -127,14 +131,30 @@ namespace DoubleTapRunner
             currentlyRunning = false;
             return true;
         }
+        
+        public override void OnPreferencesSaved()
+        {
+            ApplySettings();
+        }
 
-        public override void OnModSettingsApplied()
+        public override void OnPreferencesLoaded()
         {
             ApplySettings();
         }
 
         public override void OnUpdate()
         {
+            #if DEBUG
+                if (Input.GetKeyDown(KeyCode.O))
+                {
+                    LocomotionInputController locomotion = Utilities.GetLocalVRCPlayer()?.GetComponent<LocomotionInputController>();
+                    if (locomotion == null) return;
+                    MelonLogger.Msg(
+                        $"Motionstate Floats: {locomotion.field_Public_Single_0} {locomotion.field_Public_Single_1} {locomotion.field_Public_Single_2} {locomotion.field_Public_Single_3} {locomotion.field_Public_Single_4} {locomotion.field_Public_Single_5} {locomotion.field_Public_Single_6} ");
+                }
+            #endif
+            
+            
             if (!activeSettings.Enabled
                 || !worldAllowed) return;
 
@@ -203,7 +223,7 @@ namespace DoubleTapRunner
         {
             // Disallow until proven otherwise
             worldAllowed = false;
-            if(!modLoadedCorrectly) yield break;
+            if (!modLoadedCorrectly) yield break;
 
             string worldId = RoomManager.field_Internal_Static_ApiWorld_0.id;
 
@@ -218,12 +238,22 @@ namespace DoubleTapRunner
                 {
                     case "allowed":
                         worldAllowed = true;
+                    #if DEBUG
+                        MelonLogger.Msg("World Allowed - Emm");
+                    #endif
                         yield break;
 
                     case "denied":
                         worldAllowed = false;
+                    #if DEBUG
+                        MelonLogger.Msg("World Denied - Emm");
+                    #endif
                         yield break;
                 }
+
+        #if DEBUG
+            MelonLogger.Msg("No Result From Emm");
+        #endif
 
             // no result from server or they're currently dead
             // Check tags then
@@ -238,10 +268,17 @@ namespace DoubleTapRunner
                             {
                                 if (worldTag.IndexOf("game", StringComparison.OrdinalIgnoreCase) == -1) continue;
                                 worldAllowed = false;
+                            #if DEBUG
+                                MelonLogger.Msg("World Denied");
+                            #endif
                                 break;
                             }
                         }),
                 disableCache: false);
+
+        #if DEBUG
+            MelonLogger.Msg("World Allowed");
+        #endif
         }
 
         private static void JoinedRoomPatch(string __0, float __1)
@@ -253,48 +290,48 @@ namespace DoubleTapRunner
 
         private void ApplySettings()
         {
-            activeSettings.Enabled = MelonPrefs.GetBool(SettingsCategory, nameof(Settings.Enabled));
-            activeSettings.SpeedMultiplier = MelonPrefs.GetFloat(SettingsCategory, nameof(Settings.SpeedMultiplier));
-            activeSettings.DoubleClickTime = MelonPrefs.GetFloat(SettingsCategory, nameof(Settings.DoubleClickTime));
+            activeSettings.Enabled = MelonPreferences.GetEntryValue<bool>(SettingsCategory, nameof(Settings.Enabled));
+            activeSettings.SpeedMultiplier = MelonPreferences.GetEntryValue<float>(SettingsCategory, nameof(Settings.SpeedMultiplier));
+            activeSettings.DoubleClickTime = MelonPreferences.GetEntryValue<float>(SettingsCategory, nameof(Settings.DoubleClickTime));
 
-            if (Enum.TryParse(MelonPrefs.GetString(SettingsCategory, nameof(Settings.Forward)), out KeyCode forward))
+            if (Enum.TryParse(MelonPreferences.GetEntryValue<string>(SettingsCategory, nameof(Settings.Forward)), out KeyCode forward))
             {
                 activeSettings.Forward = forward;
             }
             else
             {
-                MelonLogger.LogError("Failed to parse KeyCode Forward");
+                MelonLogger.Error("Failed to parse KeyCode Forward");
             }
             
-            if (Enum.TryParse(MelonPrefs.GetString(SettingsCategory, nameof(Settings.Backward)), out KeyCode backward))
+            if (Enum.TryParse(MelonPreferences.GetEntryValue<string>(SettingsCategory, nameof(Settings.Backward)), out KeyCode backward))
             {
                 activeSettings.Backward = backward;
             }
             else
             {
-                MelonLogger.LogError("Failed to parse KeyCode Backward");
+                MelonLogger.Error("Failed to parse KeyCode Backward");
             }
             
-            if (Enum.TryParse(MelonPrefs.GetString(SettingsCategory, nameof(Settings.Left)), out KeyCode left))
+            if (Enum.TryParse(MelonPreferences.GetEntryValue<string>(SettingsCategory, nameof(Settings.Left)), out KeyCode left))
             {
                 activeSettings.Left = left;
             }
             else
             {
-                MelonLogger.LogError("Failed to parse KeyCode Left");
+                MelonLogger.Error("Failed to parse KeyCode Left");
             }
             
-            if (Enum.TryParse(MelonPrefs.GetString(SettingsCategory, nameof(Settings.Right)), out KeyCode right))
+            if (Enum.TryParse(MelonPreferences.GetEntryValue<string>(SettingsCategory, nameof(Settings.Right)), out KeyCode right))
             {
                 activeSettings.Right = right;
             }
             else
             {
-                MelonLogger.LogError("Failed to parse KeyCode Right");
+                MelonLogger.Error("Failed to parse KeyCode Right");
             }
 
-            activeSettings.AxisDeadZone = MelonPrefs.GetFloat(SettingsCategory, nameof(Settings.AxisDeadZone));
-            activeSettings.AxisClickThreshold = MelonPrefs.GetFloat(SettingsCategory, nameof(Settings.AxisClickThreshold));
+            activeSettings.AxisDeadZone = MelonPreferences.GetEntryValue<float>(SettingsCategory, nameof(Settings.AxisDeadZone));
+            activeSettings.AxisClickThreshold = MelonPreferences.GetEntryValue<float>(SettingsCategory, nameof(Settings.AxisClickThreshold));
 
             SetLocomotion();
         }
@@ -314,18 +351,18 @@ namespace DoubleTapRunner
             if (currentlyRunning)
             {
                 grabbedWorldSettings = true;
-                walkSpeed = locomotion.walkSpeed;
-                runSpeed = locomotion.runSpeed;
-                strafeSpeed = locomotion.strafeSpeed;
+                walkSpeed = locomotion.field_Public_Single_1;
+                runSpeed = locomotion.field_Public_Single_0;
+                strafeSpeed = locomotion.field_Public_Single_2;
             }
 
             // to stop being unable to move randomly...
             if (!grabbedWorldSettings) return;
             
             float multiplier = activeSettings.Enabled && currentlyRunning ? activeSettings.SpeedMultiplier : 1f;
-            locomotion.walkSpeed = walkSpeed * multiplier;
-            locomotion.runSpeed = runSpeed * multiplier;
-            locomotion.strafeSpeed = strafeSpeed * multiplier;
+            locomotion.field_Public_Single_1 = walkSpeed * multiplier;
+            locomotion.field_Public_Single_0 = runSpeed * multiplier;
+            locomotion.field_Public_Single_2 = strafeSpeed * multiplier;
         }
 
         private struct Settings
